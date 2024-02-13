bd5c7a43f3f54dd1b9190314d957a5fb56eefab4
Wizaator <146935098+Wizaator@users.noreply.github.com>
2024-02-13 14:40:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function ano(){
+    document.getElementById("text").innerHTML="Yeeey!";
+    document.getElementById("obrazek").src="img/happy.png";
+    document.getElementById("obsah").style.opacity= 1;
+    document.getElementById("obsah2").innerHTML="Ahoj, chtěl bych ti tady říct jen pár věcí, které mám na srci. Jsi teď pro mě opravdu důležitá osoba (jak jde asi vidět) a záleží mi na tobě, proto jsem taky připravil tuto stránku, abych ti udělal radost a by jsi věděla jak mi na tobě záleží &#9825 ."
+}
+
+function ne(){
+    document.getElementById("text").innerHTML="NEEEE";
+    document.getElementById("obrazek").src="img/sad.png";
+    document.getElementById("obsah").style.opacity= 1;
+    document.getElementById("obsah2").innerHTML="Ahoj, mrzí mě že to tak cítíš, každopádně já se i přes to rozhodl tento web udělat, protože jsem ze sebe chtěl dostat emoce, které tam už nějakou tu chivlku jsou &#9825 ."
+}
